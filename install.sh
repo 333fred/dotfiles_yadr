69321ae547fa2225406ac9193571066cd49383a1
Fredric Silberberg <333fred@users.noreply.github.com>
2016-01-04 12:30:31 -0500

    Use HTTPS instead of SSH so I don't have to be logged in.

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 if [ ! -d "$HOME/.yadr" ]; then
     echo "Installing YADR for the first time"
-    git clone --depth=1 git@github.com:333fred/dotfiles_yadr.git "$HOME/.yadr"
+    git clone --depth=1 https://github.com/333fred/dotfiles_yadr.git "$HOME/.yadr"
     cd "$HOME/.yadr"
     [ "$1" = "ask" ] && export ASK="true"
     rake install
