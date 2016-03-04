d08a7b90c0b0556d5cd5ddb4b4312897aa71ba97
Fredric Silberberg <fredric@fsilberberg.com>
2016-03-03 22:10:47 -0500

    Allow for checkout of a specific branch

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@
 if [ ! -d "$HOME/.yadr" ]; then
     echo "Installing YADR for the first time"
     git clone --depth=1 https://github.com/333fred/dotfiles_yadr.git "$HOME/.yadr"
+    if [ ! -z "$1" ]; then
+        git checkout "$1"
+    fi
     cd "$HOME/.yadr"
     [ "$1" = "ask" ] && export ASK="true"
     rake install
