8bffa6c91b80864921fe272c0034bf4c0d2bba7c
Fredric Silberberg <fredric@fsilberberg.com>
2016-03-03 22:42:28 -0500

    Updated the install script to allow for cloning a specific branch.

--- install.sh
+++ install.sh
@@ -2,9 +2,10 @@
 
 if [ ! -d "$HOME/.yadr" ]; then
     echo "Installing YADR for the first time"
-    git clone --depth=1 https://github.com/333fred/dotfiles_yadr.git "$HOME/.yadr"
     if [ ! -z "$1" ]; then
-        git checkout "$1"
+        git clone -b "$1" --depth=1 https://github.com/333fred/dotfiles_yadr.git "$HOME/.yadr"
+    else
+        git clone --depth=1 https://github.com/333fred/dotfiles_yadr.git "$HOME/.yadr"
     fi
     cd "$HOME/.yadr"
     [ "$1" = "ask" ] && export ASK="true"
