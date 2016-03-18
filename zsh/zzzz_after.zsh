c5728879b1c9dbb288e2b28ab32f8edb3d05c0af
Fredric Silberberg <fredric@fsilberberg.com>
2016-03-17 23:00:44 -0400

    Continued updates for gnu stow

--- zsh/zzzz_after.zsh
+++
@@ -1,7 +0,0 @@
-export PYTHONSTARTUP="$HOME/.pythonrc"
-# Load any custom after code
-if [ -d $HOME/.zsh.after/ ]; then
-  if [ "$(ls -A $HOME/.zsh.after/)" ]; then
-    for config_file ($HOME/.zsh.after/*.zsh) source $config_file
-  fi
-fi
